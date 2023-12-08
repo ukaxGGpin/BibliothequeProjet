8e02020fc82a4b7d279c85fe2331f5607302da0b
sio1 <sansanlanta@gmail.com>
2023-12-08 17:10:30 +0100

    Formulaire + livvre recherche

--- accueil.php
+++ accueil.php
@@ -5,35 +5,73 @@
   <title>Bibliotheque LSB</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
   <div class="container">
-
-            <?php
-              include("entete.html");
-            ?>
-
+    <!-- En-tête -->
+    <?php
+      include("entete.html");
+    ?>
+    <!-- Corps page principale -->
       <div class="row">
-    
-          <div class="col-md-8">
-          <div class="carousel-item">
-  <img src="bleak.jpg" alt="Los Angeles">
-  <div class="carousel-caption">
-    <h3>BLEAK/h3>
-    <p>Crée par le célebre YouTuber Squeezie</p>
-  </div>
-</div>
+        <div class="col-md-8">
+        <!-- Carousel -->
+          <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+            <!-- Indicators/dots -->
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+              <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- The slideshow/carousel -->
+            <div class="text-center">
+              <h2>Derniere acquisition</h2>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="images/bleak.jpg" alt="Bleak" class="d-block" style="width: 800px; length: 500px;">
+                <div class="carousel-caption">
+                  <h3>Squezzie</h3>
+                  <p>Livre d'horreur du célèbre Youtubeur Squezzie</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="images/NoetMoi.jpg" alt="NoetMoi" class="d-block" style="width: 800px;length: 80px;">
+                <div class="carousel-caption">
+                  <h3>Delphine de Vigan</h3>
+                  <p>Histoire passionnate du jeune fille et d'une sdf</p>
+                </div> 
+              </div>
+              <div class="carousel-item">
+                <img src="images/lesMisérables.jpg" alt="LesMisérables" class="d-block" style="width: 800px;length: 300px;">
+                <div class="carousel-caption">
+                  <h3>Victor Hgo</h3>
+                  <p>L'un des livre les plus prisés du célèbre écrivain Victor Hugo</p>
+                </div>  
+              </div>
+            </div>
+
+            <!-- Left and right controls/icons -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+              <span class="carousel-control-next-icon"></span>
+            </button>
           </div>
+        </div>
         
-          <div class="col-md-4">
-        
-            <?php
-              include("authentification.php");
-            ?>
-          </div>
+        <div class="col-md-4">
+      
+          <?php
+            include("authentification.php");
+          ?>
+        </div>
       </div>
   </div>
 
@@ -76,19 +114,19 @@
     
        
 
-      </form>
-    </div>
+  </form>
+</div>
+-->
   </div>
   <footer>
-  <div class="container-fluid mt-3">
-    <div class="texte-center col-sm-12">
+  <div class="container-fluid ">
+    <div class="texte-center">
         <p>
         <h5 class="texte-center">La Bibliotheque LSB est fermée au public j'usqu'a nouvel ordre. Mais il vous est possible de réservé et
           retirer vos livre via notre service de Bibliotheque en ligne!</h5>
         </p>
       </div>
     </footer>
-  -->
 
 </body>
 
--- authentification.php
+++ authentification.php
@@ -1,11 +1,35 @@
+<?php
+session_start();
+require_once('connexionSql.php');
+if (isset($_POST["mail"])) {
+  $mail=$_POST["mail"];
+  $Mdp=$_POST["Mdp"];
+  $stmt = $connexion->prepare("SELECT * FROM utilisateur where mel=:mail AND motdepasse=:Mdp");
+  $stmt->bindValue(":mail", $mail); // pas de troisième paramètre STR par défaut
+  $stmt->bindValue(":Mdp", $Mdp); // idem
+  $stmt->setFetchMode(PDO::FETCH_OBJ);
+  // Les résultats retournés par la requête seront traités en 'mode' objet
+  $stmt->execute();
+  // Parcours des enregistrements retournés par la requête : premier, deuxième…
+
+  if($enregistrement = $stmt->fetch())
+  {
+    echo "Bienvenue.$mail."
+  }
+}
+?>
+<table>
 <h2>Se connecter</h2>
-      <form action="">
+<form action="authentification.php" method="post">
+          <tr>
           <label for="email">Mail:</label>
           <input type="email" class="form-control" id="email" placeholder="Enter email" name="mail">
           <label for="pwd">Mot de passe:</label>
           <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="Mdp">
           <label class="form-check-label">
-            <input class="form-check-input" type="checkbox" name="remember"> Remember me
+            <input class="form-check-input" type="checkbox" name="remember"> se souvenir de moi
           </label><br>
         <button type="submit" class="btn btn-primary">se connecter</button>
+      </tr>
+    </table>  
       </form>
