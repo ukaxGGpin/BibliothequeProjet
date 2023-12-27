8afe9a93303fea001d07c42027c00d3731bab05a
Betsaleel Lanta <sansanlanta08@gmail.com>
2023-12-27 07:02:03 +0100

    Essaie de regler le probleme du boutton se connecter/le boutton recherche

--- authentification.php
+++ authentification.php
@@ -4,31 +4,44 @@ require_once('connexionSql.php');
 // Vérifiez si l'utilisateur est déjà connecté
 if(isset($_SESSION['utilisateur_connecte'])) {
     // Affichez les informations de l'utilisateur connecté et le bouton de déconnexion
+    echo'<div class="text-center"';
+    echo "<table class='rounded-table'>";
+    echo "<tr><td>";
     echo "Bienvenue " . $_SESSION['nom_utilisateur'] . "<br>";
     echo "Email: " . $_SESSION['email_utilisateur'] . "<br>";
     echo '<form action="deconnexion.php" method="post">';
     echo '<button type="submit" class="btn btn-primary">Se déconnecter</button>';
     echo '</form>';
+    echo "</td></tr>";
+    echo "</table>";
+    echo "</div>";
+
 } else {
     // Si l'utilisateur n'est pas connecté, affichez le formulaire de connexion
-    echo '<table>';
+    echo'<div class="text-center"';
+    echo '<table class="rounded-table">';
+    echo '<tr><td>';
     echo '<h2>Se connecter</h2>';
     echo '<form method="post">';
-    echo '<tr>';
     echo '<label for="email">Mail:</label>';
-    echo '<input type="email" class="form-control" id="email" placeholder="Entrer votre mail" name="mail">';
+    echo '<input type="email" class="form-control" id="email" placeholder="Entrer votre mail" name="mail"><br>';
     echo '<label for="pwd">Mot de passe:</label>';
-    echo '<input type="password" class="form-control" id="pwd" placeholder="Entrer votre mot de passe" name="Mdp">';
+    echo '<input type="password" class="form-control" id="pwd" placeholder="Entrer votre mot de passe" name="Mdp"><br>';
     echo '<label class="form-check-label">';
     echo '<input class="form-check-input" type="checkbox" name="remember"> se souvenir de moi';
     echo '</label><br>';
-    echo '<button type="submit" class="btn btn-primary" name="submit">se connecter</button>';
-    echo '</tr>';
+    echo '<div class="d-grid gap-2">';
+    echo '<button type="submit" class="btn btn-primary" name="submit">Se connecter</button>';
+    echo '</div>';
     echo '</form>';
     echo '<form action="AjouteUnMembre.php">';
+    echo '<div class="d-grid gap-2">';
     echo '<button type="submit" class="btn btn-primary">Créer un compte</button>';
+    echo '</div>';
     echo '</form>';
+    echo '</td></tr>';
     echo '</table>';
+    echo "</div>";
 
     if (isset($_POST['submit'])) {
         if (isset($_POST['mail']) && isset($_POST['Mdp'])) {
