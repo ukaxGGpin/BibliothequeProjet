133db0107c4cb78373baa8f1e8b7746574ffc0ac
Betsaleel Lanta <sansanlanta08@gmail.com>
2023-12-27 06:36:11 +0100

    Visualisation accueil /recherche.php

--- recherche.php
+++ recherche.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="style.css">
     </head>
     
     <body>
@@ -42,11 +43,12 @@ if (isset($_POST['recherche']) && !empty($_POST['recherche'])) {
       if ($resultat->rowCount() > 0) {
           echo '<div class="text-center">';
           echo "<h5 class= text_center>Livres de l'auteur $nom_auteur :</h2>";
+          echo "<ul>";
           echo "</div>";
           while ($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
-              echo "<a href" .  $row['titre'] .  "</a>";
+              echo "<li>" . $row['titre'] . "</li>";
           }
-          
+          echo "</ul>";
       } else {
           echo "Aucun livre trouvé pour cet auteur.";
       }
@@ -56,6 +58,8 @@ if (isset($_POST['recherche']) && !empty($_POST['recherche'])) {
   }
 }
    ?>
+   </div>
+   </div>
         <div class="col-md-4">
       
           <?php
