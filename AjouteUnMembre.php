e7dcdf92fcfca064937fe3f6ea601004ec933d61
Betsaleel Lanta <sansanlanta08@gmail.com>
2023-12-27 07:37:52 +0100

    Amelioration visuelle

--- AjouteUnMembre.php
+++ AjouteUnMembre.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -45,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
-
+<div class="text-center">
 <table class=" table-bordered ">
     <tr>
   <form  method="post">
@@ -88,5 +89,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </form>
 </tr>
 </table>
+</div>
 </body>
 </html>
