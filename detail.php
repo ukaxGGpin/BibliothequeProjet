b381fd20e379bcdae3cf9b9777ba66a1fbb65f3f
Betsaleel Lanta <sansanlanta08@gmail.com>
2024-01-04 20:30:33 +0100

    detail.php /lien pour les livre/division du tableau pour eviter les changement de position

---
+++ detail.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Bibliotheque LSB</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    </div>
+    <div class="container-fluid">
+        <!-- En-tête -->
+        <?php
+        include("entete.html");
+        ?>
+        <!-- Corps page principale -->
+        <div class="row">
+            <div class="col-md-8">
+
+            </div>
+            <div class="col-md-4">
+
+                <?php
+                include("authentification.php");
+                ?>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+
+</html>
--- recherche.php
+++ recherche.php
@@ -1,72 +1,66 @@
 <!DOCTYPE html>
 <html lang="fr">
-    
-    <head>
-        <title>Bibliotheque LSB</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    
-    <body>
-        <div class="text-center alert alert-danger container-fluid ">
-              <p>
-              <h5 >ATTENTION ! La Bibliothèque LSB est fermée au public j'usqu'a nouvel ordre. Mais il vous est possible de réservé et
-                retirer vos livre via notre service de Bibliotheque en ligne!</h5>
-              </p>
-            </div>
-    </div>
+
+<head>
+  <title>Bibliotheque LSB</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  </div>
   <div class="container-fluid">
     <!-- En-tête -->
     <?php
-      include("entete.html");
+    include("entete.html");
     ?>
     <!-- Corps page principale -->
-      <div class="row">
-        <div class="col-md-8">
-            <?php
-            //connexion a base SQL
-  require_once('connexionSql.php');           
-            // Vérifier si une recherche a été effectuée
-if (isset($_POST['recherche']) && !empty($_POST['recherche'])) {
-  $nom_auteur = $_POST['recherche'];
+    <div class="row">
+      <div class="col-md-8">
+        <?php
+        //connexion a base SQL
+        require_once('connexionSql.php');
+        // Vérifier si une recherche a été effectuée
+        if (isset($_POST['recherche']) && !empty($_POST['recherche'])) {
+          $nom_auteur = $_POST['recherche'];
 
-  // Préparer la requête SQL pour rechercher les livres liés à l'auteur recherché
-  $requete = "SELECT livre.titre FROM livre INNER JOIN auteur ON livre.noauteur = auteur.noauteur WHERE auteur.nom = '$nom_auteur'";
-  // Exécuter la requête
-  $resultat = $connexion->query($requete);
+          // Préparer la requête SQL pour rechercher les livres liés à l'auteur recherché
+          $requete = "SELECT livre.titre FROM livre INNER JOIN auteur ON livre.noauteur = auteur.noauteur WHERE auteur.nom = '$nom_auteur'";
+          // Exécuter la requête
+          $resultat = $connexion->query($requete);
 
-  if ($resultat) {
-      // Afficher les résultats de la recherche
-      if ($resultat->rowCount() > 0) {
-          echo '<div class="text-center">';
-          echo "<h5 class= text_center>Livres de l'auteur $nom_auteur :</h2>";
-          echo "<ul>";
-          echo "</div>";
-          while ($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
-              echo "<li>" . $row['titre'] . "</li>";
+          if ($resultat) {
+            // Afficher les résultats de la recherche
+            if ($resultat->rowCount() > 0) {
+              echo '<div class="text-center">';
+              echo "<h5 class= text_center>Livres de l'auteur $nom_auteur :</h2>";
+              echo "<ul>";
+              echo "</div>";
+              while ($row = $resultat->fetch(PDO::FETCH_ASSOC)) {
+                echo "<li><a href='detail.php'>" . $row['titre'] . "</a></li>";
+              }
+              echo "</ul>";
+              // Si aucun livre trouver
+            } else {
+              echo "Aucun livre trouvé pour cet auteur.";
+            }
+          } else {
+            echo "Erreur dans la requête : " . $connexion->error;
           }
-          echo "</ul>";
-      } else {
-          echo "Aucun livre trouvé pour cet auteur.";
-      }
-      
-    } else {
-      echo "Erreur dans la requête : " . $connexion->error;
-  }
-}
-   ?>
-   </div>
-   </div>
-        <div class="col-md-4">
-      
-          <?php
-            include("authentification.php");
-          ?>
-        </div>
+        }
+        ?>
       </div>
+      <div class="col-md-4">
+
+        <?php
+        include("authentification.php");
+        ?>
+      </div>
+    </div>
+  </div>
   </div>
 </body>
 
